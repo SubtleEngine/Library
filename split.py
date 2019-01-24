08ea0c3d5dc6413847022ac14a83345e2a1374d9
Subtle Engine <subtleengine@gmail.com>
2019-01-24 16:24:23 +0000

    Added tags as “keyword” param to individual JSON files

--- split.py
+++ split.py
@@ -1,7 +1,11 @@
 import json
+import pandas as pd
 
 data = json.load(open("Subtle Engine.json"))
+tags = pd.read_csv("Subtle Engine.csv", index_col="Key")["Manual Tags"].to_dict()
+
 for item in data:
     key = item["id"].split("/")[-1]
+    tagged_item = dict(**item, **{"keyword": [t.strip() for t in tags[key].split(";")]})
     with open("Individual JSON/"+key+".json", "w") as f:
-        json.dump(item, f)
+        json.dump(tagged_item, f)
